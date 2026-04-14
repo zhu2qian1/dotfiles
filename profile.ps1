f961d931ec5695f2de4afbb66b9dddd2f606ad0b
zhu2qian1 <16220156+zhu2qian1@users.noreply.github.com>
2026-04-14 13:54:41 +0900

    add profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module PSFzf
+Enable-PsFzfAliases
+Import-Module ZLocation
+
+$Env:Editor='gvim'
+
+function Prompt() {
+    Write-Host ""
+    Write-Host -NoNewLine -ForegroundColor Blue "$env:username@$env:computername"
+    Write-Host " $(Convert-Path $PWD)"
+    return "PS> "
+}
+
+if (Get-Command "eza" -ErrorAction SilentlyContinue) {
+    function Invoke-EzaLla {
+        eza --all --icons --long --time-style long-iso $args
+    }
+    Set-Alias lla Invoke-EzaLla
+    function Invoke-EzaLl {
+        eza       --icons --long --time-style long-iso $args
+    }
+    Set-Alias ll Invoke-EzaLl
+}
+

3b37a8fc968c0aaf9fbdb90f5d1a10aba92e4504
kazuma takemori <takemori@kmkworld.com>
2026-04-14 14:06:54 +0900

    fix profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,20 +1,17 @@
 ﻿$Env:Editor='gvim'
 
 function Prompt() {
-    Write-Output ""
+    Write-Host ""
     Write-Host -NoNewLine -ForegroundColor Blue "$env:username@$env:computername"
     Write-Host " $(Convert-Path $PWD)"
     return "PS> "
 }
+Set-Alias el explorer.exe
 
 if (Get-Command "eza" -ErrorAction SilentlyContinue) {
-    function Invoke-EzaLla {
-        eza --all --icons --long --time-style long-iso $args
-    }
+    function Invoke-EzaLla { eza --all --icons --long --header --time-style long-iso $args }
     Set-Alias lla Invoke-EzaLla
-    function Invoke-EzaLl {
-        eza       --icons --long --time-style long-iso $args
-    }
+    function Invoke-EzaLl  { eza       --icons --long --header --time-style long-iso $args }
     Set-Alias ll Invoke-EzaLl
 }
 

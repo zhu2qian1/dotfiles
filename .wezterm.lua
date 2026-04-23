648b586bc9abfcb117f87c374847bc500d4db27c
kazuma takemori <takemori@kmkworld.com>
2026-04-23 09:44:43 +0900

    wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,10 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+config.font = wezterm.font('UDEV Gothic NFLG')
+--config.color_scheme = 'DimmedMonokai'
 config.color_scheme = 'AdventureTime'
+config.font_size = 11
+config.default_prog = { 'pwsh.exe' }
 
 return config

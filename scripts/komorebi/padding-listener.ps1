d06cd530dba8724c5b223047e1912098168a3249
zhu2qian1 <16220156+zhu2qian1@users.noreply.github.com>
2026-07-20 11:50:20 +0900

    komorebi event listener

--- scripts/komorebi/padding-listener.ps1
+++ scripts/komorebi/padding-listener.ps1
@@ -12,7 +12,8 @@ Write-Host "クライアントが接続しました。"
 $reader = New-Object System.IO.StreamReader($pipeServer)
 
 $targetWorkspaceIndex = 0
-$logSkippingEventTypes = @("Uncloak", "Cloak")
+$logSkippingEventTypes = @("Uncloak", "Cloak", "TitleUpdate", "AddSubscriberPipe", "FocusChange", "FocusWindow")
+$loggingEventTypes = @('Destroy', 'Hide')
 
 try {
     while ($true) {
@@ -26,18 +27,22 @@ try {
         # Write-Output $j
         # SKIP
         if ($logSkippingEventTypes -contains $j.event.type) {
-            Write-Output " --- SKIP LOGGING --- "
+            Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') DEBUG: SKIPPING EVENT ($($j.event.type))" -ForegroundColor 'DarkGray'
             continue
         }
-        Write-Output " --- BEGIN log --- "
-        if ($j.event.type -eq 'FocusWorkspaceNumber' -and $j.event.type -eq $targetWorkspaceIndex) {
-            Write-Output " --- BEGIN event.type=FocusWorkspaceNumber: 0 --- "
-            Write-Output $j.state
-            Write-Output " --- END event.type=FocusWorkspaceNumber: 0 --- "
+        if ($loggingEventTypes -contains $j.event.type) {
+            Write-Host -ForegroundColor 'Blue'      "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') DEBUG: MATCH"
+            Write-Host -ForegroundColor 'DarkGreen' "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') DEBUG: type    = $($j.event.type) "
+            Write-Host -ForegroundColor 'DarkGreen' "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') DEBUG: content = $($j.event.content)"
+            continue
         }
-        Write-Output $j.event.type
-        Write-Output $j.event.content
-        Write-Output " --- END log --- "
+        # if ($j.event.type -eq 'FocusWorkspaceNumber' -and $j.event.type -eq $targetWorkspaceIndex) {
+        #     Write-Output " --- BEGIN event.type=FocusWorkspaceNumber: 0 --- "
+        #     Write-Output $j.state
+        #     Write-Output " --- END event.type=FocusWorkspaceNumber: 0 --- "
+        # }
+        Write-Host -ForegroundColor 'Cyan' "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') DEBUG: type    = $($j.event.type) "
+        Write-Host -ForegroundColor 'Cyan' "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') DEBUG: content = $($j.event.content)"
     }
 }
 finally {

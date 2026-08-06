29bf0d296098305b85585e01991be41104448262
takemori <takemori@kmkworld.com>
2026-08-06 12:29:34 +0900

    profile

--- .config/PowerShell/profile.ps1
+++ .config/PowerShell/profile.ps1
@@ -2,6 +2,7 @@
 . "$HOME\.config\PowerShell\prompt.ps1"
 . "$HOME\.config\PowerShell\aliases.ps1"
 
+. "$HOME\.config\PowerShell\fzf.ps1"
 . "$HOME\.config\PowerShell\nvim.ps1"
 . "$HOME\.config\PowerShell\yazi.ps1"
 . "$HOME\.config\PowerShell\komorebi.ps1"

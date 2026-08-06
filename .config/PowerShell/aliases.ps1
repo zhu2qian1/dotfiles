affcad9c02d79a411ed43155be751a811a53f964
takemori <takemori@kmkworld.com>
2026-08-06 12:17:05 +0900

    profile nvim

--- .config/PowerShell/aliases.ps1
+++ .config/PowerShell/aliases.ps1
@@ -76,31 +76,3 @@ if (Get-Command "lazygit" -ErrorAction SilentlyContinue) {
     Set-Alias lg lazygit
 }
 
-# nvim
-if (Get-Command "nvim" -ErrorAction SilentlyContinue) {
-    $env:NVIM_PROFILE='lite';
-    Set-Alias v nvim
-
-    function Set-Nvim-Profile($Mode, [switch] $NoOutput) {
-        if ($Mode -imatch '^i.{0,2}$') {
-            $env:NVIM_PROFILE='ide';
-        } elseif ($Mode -imatch '^l.{0,3}$') {
-            $env:NVIM_PROFILE='lite';
-        } else {
-            Write-Warning "Accepted values: '^i.{0,2}$',  '^l.{0,3}$'";
-        }
-
-        if (-not $NoOutput) {
-            Write-Host "env:NVIM_PROFILE=$env:NVIM_PROFILE";
-        }
-
-        return;
-    }
-    Set-Alias snp Set-Nvim-Profile
-
-    function Start-Nvim-Ide  { Set-Nvim-Profile ide;  nvim $Args }
-    Set-Alias vide Start-Nvim-Ide
-
-    function Start-Nvim-Lite { Set-Nvim-Profile lite; nvim $Args }
-    Set-Alias vli Start-Nvim-Lite
-}
---
+++ .config/PowerShell/nvim.ps1
@@ -0,0 +1,30 @@
+# nvim
+if (-not (Get-Command "nvim" -ErrorAction SilentlyContinue)) {
+    return;
+}
+
+$env:NVIM_PROFILE='lite';
+Set-Alias v nvim
+
+function Set-Nvim-Profile($Mode, [switch] $NoOutput) {
+    if ($Mode -imatch '^i.{0,2}$') {
+        $env:NVIM_PROFILE='ide';
+    } elseif ($Mode -imatch '^l.{0,3}$') {
+        $env:NVIM_PROFILE='lite';
+    } else {
+        Write-Warning "Accepted values: '^i.{0,2}$',  '^l.{0,3}$'";
+    }
+
+    if (-not $NoOutput) {
+        Write-Host "env:NVIM_PROFILE=$env:NVIM_PROFILE";
+    }
+
+    return;
+}
+Set-Alias snp Set-Nvim-Profile
+
+function Start-Nvim-Ide  { Set-Nvim-Profile ide;  nvim $Args }
+Set-Alias vide Start-Nvim-Ide
+
+function Start-Nvim-Lite { Set-Nvim-Profile lite; nvim $Args }
+Set-Alias vli Start-Nvim-Lite

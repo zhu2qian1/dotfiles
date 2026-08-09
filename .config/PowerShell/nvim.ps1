f99a231598d91d8811fc085e69b2a54fc51dfbf2
zhu2qian1 <16220156+zhu2qian1@users.noreply.github.com>
2026-08-09 11:14:39 +0900

    profile

--- .config/PowerShell/nvim.ps1
+++ .config/PowerShell/nvim.ps1
@@ -23,8 +23,8 @@ function Set-Nvim-Profile($Mode, [switch] $NoOutput) {
 }
 Set-Alias snp Set-Nvim-Profile
 
-function Start-Nvim-Ide  { Set-Nvim-Profile ide;  nvim $Args }
+function Start-Nvim-Ide  { Set-Nvim-Profile ide -NoOutput;  nvim $Args }
 Set-Alias vide Start-Nvim-Ide
 
-function Start-Nvim-Lite { Set-Nvim-Profile lite; nvim $Args }
+function Start-Nvim-Lite { Set-Nvim-Profile lite -NoOutput; nvim $Args }
 Set-Alias vli Start-Nvim-Lite
